6abc596d392b5805d8b67439651d26204454f179
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-01 08:16:52 -0600

    Add Yield Keyword within MyList initializer

--- my_list.rb
+++ my_list.rb
@@ -8,7 +8,7 @@ class MyList
   include MyEnumerable
 
   def each?
-    @list
+    yield @list
   end
 end
 
@@ -17,3 +17,4 @@ list = MyList.new('ant', 'doggo', 'parrot')
 puts(list.all? { |n| n.length == 3 }) # => should return false
 puts(list.any? { |n| n.length == 6 }) # => should return true
 print(list.filter { |n| n.length <= 5 }) # => should return ['ant', 'doggo']
+
