3580f1c620565f66c97ea555e9e67edf857e0a70
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-09-01 08:25:40 -0600

    Add puts to show corretly on console the method result

--- my_list.rb
+++ my_list.rb
@@ -15,5 +15,5 @@ end
 list = MyList.new('ant', 'doggo', 'parrot')
 puts(list.all? { |n| n.length == 3 }) # => should return false
 puts(list.any? { |n| n.length == 6 }) # => should return true
-print(list.filter { |n| n.length <= 5 }) # => should return ['ant', 'doggo']
+puts print(list.filter { |n| n.length <= 5 }) # => should return ['ant', 'doggo']
 
